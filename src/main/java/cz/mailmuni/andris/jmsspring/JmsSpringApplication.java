775c327e71808cd39d14f410a8d74e36d9498d4d
Andras Urge <andris@mail.muni.cz>
2020-04-25 11:28:24 +0200

    Initial project created

---
+++ src/main/java/cz/mailmuni/andris/jmsspring/JmsSpringApplication.java
@@ -0,0 +1,13 @@
+package cz.mailmuni.andris.jmsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/mailmuni/andris/jmsspring/JmsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.mailmuni.andris.jmsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
